dbe50f0140f491d8795efa13f02bf7b4dd6bf947
anyesgray38 <anyesgray38@gmail.com>
2026-09-02 17:29:11 -0400

    Export Fleet config validation

--- fleet/__init__.py
+++ fleet/__init__.py
@@ -1,6 +1,17 @@
 """Distributed AEGIS Fleet execution primitives."""
+from .config import FleetConfigError, validate_fleet_config
 from .node import FleetNode
 from .policy import NetworkPolicy, NetworkPolicyDenied, NetworkRequest
 from .scheduler import FleetScheduler, NodeScore, Workload
 
-__all__ = ["FleetNode", "FleetScheduler", "NetworkPolicy", "NetworkPolicyDenied", "NetworkRequest", "NodeScore", "Workload"]
+__all__ = [
+    "FleetConfigError",
+    "FleetNode",
+    "FleetScheduler",
+    "NetworkPolicy",
+    "NetworkPolicyDenied",
+    "NetworkRequest",
+    "NodeScore",
+    "Workload",
+    "validate_fleet_config",
+]
