e2865cff065474259a4387cc857a5702630b1649
anyesgray38 <anyesgray38@gmail.com>
2026-09-02 17:24:25 -0400

    Export Fleet identity and execution primitives

--- fleet/__init__.py
+++ fleet/__init__.py
@@ -2,5 +2,14 @@
 from .node import FleetNode
 from .policy import NetworkPolicy, NetworkPolicyDenied, NetworkRequest
 from .scheduler import FleetScheduler, NodeScore, Workload
+from .identity import IdentityError, NodeIdentity, sign_request, verify_request
+from .enrollment import EnrollmentAuthority, EnrollmentDenied, EnrollmentRequest
+from .attestation import CapabilityAttestation, attest_node
+from .transport import ExecutionRequest, SignedTransport, TransportDenied
 
-__all__ = ["FleetNode", "FleetScheduler", "NetworkPolicy", "NetworkPolicyDenied", "NetworkRequest", "NodeScore", "Workload"]
+__all__ = [
+    "FleetNode", "FleetScheduler", "NetworkPolicy", "NetworkPolicyDenied", "NetworkRequest",
+    "NodeScore", "Workload", "IdentityError", "NodeIdentity", "sign_request", "verify_request",
+    "EnrollmentAuthority", "EnrollmentDenied", "EnrollmentRequest", "CapabilityAttestation",
+    "attest_node", "ExecutionRequest", "SignedTransport", "TransportDenied",
+]
