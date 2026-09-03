f5bd7d82d129f73c8dd57a6243f0287dc728cc15
Shark223 <anyesgray38@gmail.com>
2026-09-02 21:08:54 -0400

    feat(actions): add governed action fabric

---
+++ actions/__init__.py
@@ -0,0 +1,5 @@
+"""Governed action primitives for AEGIS agents."""
+
+from .fabric import ActionContext, ActionError, ActionFabric, ActionResult
+
+__all__ = ["ActionContext", "ActionError", "ActionFabric", "ActionResult"]
