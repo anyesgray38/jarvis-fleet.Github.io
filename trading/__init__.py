2679aeef7b1fd6670496d51b08a782635694a084
Shark223 <anyesgray38@gmail.com>
2026-09-03 22:13:13 -0400

    refactor(trading): export backtest and bot fleet

--- trading/__init__.py
+++ trading/__init__.py
@@ -4,10 +4,12 @@ from .models import Candle, Direction, OrderIntent, Position, Signal
 from .smc import SMCAnalyzer
 from .risk import RiskPolicy, RiskDecision
 from .bot import TradingBot, BotState
+from .fleet import TradingBotFleet
+from .backtest import BacktestReport, Backtester, TradeResult
 from .paper import PaperBroker
 
 __all__ = [
-    "BotState", "Candle", "Direction", "OrderIntent", "PaperBroker",
-    "Position", "RiskDecision", "RiskPolicy", "SMCAnalyzer", "Signal",
-    "TradingBot",
+    "BacktestReport", "Backtester", "BotState", "Candle", "Direction",
+    "OrderIntent", "PaperBroker", "Position", "RiskDecision", "RiskPolicy",
+    "SMCAnalyzer", "Signal", "TradeResult", "TradingBot", "TradingBotFleet",
 ]
