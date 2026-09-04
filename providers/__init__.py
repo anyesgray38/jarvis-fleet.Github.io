73348d696db132473f2752a0799e5280741ce987
Shark223 <anyesgray38@gmail.com>
2026-09-04 13:23:31 -0400

    Export OpenAI-compatible model provider

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,5 +1,13 @@
 """Infrastructure providers consumed by the AEGIS control plane."""
 
 from .localai import LocalAIConfig, LocalAIError, LocalAIProvider
+from .openai_compatible import OpenAICompatibleConfig, OpenAICompatibleError, OpenAICompatibleProvider
 
-__all__ = ["LocalAIConfig", "LocalAIError", "LocalAIProvider"]
+__all__ = [
+    "LocalAIConfig",
+    "LocalAIError",
+    "LocalAIProvider",
+    "OpenAICompatibleConfig",
+    "OpenAICompatibleError",
+    "OpenAICompatibleProvider",
+]
