814a6bffa8da646e4b178fc9b716df104cecf746
Shark223 <anyesgray38@gmail.com>
2026-09-05 02:57:08 -0400

    Export model fabric components

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,12 +1,21 @@
 """Infrastructure providers consumed by the AEGIS control plane."""
 
 from .localai import LocalAIConfig, LocalAIError, LocalAIProvider
+from .model_fabric import ModelFabric
+from .model_registry import ModelRegistry, ModelRegistryError, RegisteredModel
+from .model_router import ModelRoute, ModelRouter
 from .openai_compatible import OpenAICompatibleConfig, OpenAICompatibleError, OpenAICompatibleProvider
 
 __all__ = [
     "LocalAIConfig",
     "LocalAIError",
     "LocalAIProvider",
+    "ModelFabric",
+    "ModelRegistry",
+    "ModelRegistryError",
+    "RegisteredModel",
+    "ModelRoute",
+    "ModelRouter",
     "OpenAICompatibleConfig",
     "OpenAICompatibleError",
     "OpenAICompatibleProvider",
